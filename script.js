f38392ef68ed2eb72b8d125d0080335724624c78
Mohmedhusain Vaid <hussainvaid7@gmail.com>
2020-03-02 23:04:07 -0600

    Figured the change question

--- script.js
+++ script.js
@@ -2,67 +2,82 @@ $(document).ready(function () {
 
     var questions = [{
             q: "This is question 1.",
-            a: "aaaa",
-            b: "bbb",
-            c: "ccc",
-            ans: "aaa"
+            a: ["aaaa", "bbbb", "cccc"],
+            ans: "aaaa"
         },
         {
             q: "This is question 2.",
-            a: "aaaa",
-            b: "bbb",
-            c: "ccc",
-            ans: "ccc"
+            a: ["aaaa", "bbbb", "cccc"],
+            ans: "cccc"
         }
 
     ];
     var score = 0;
     var questionIndex = 0;
-    var options= "";
+    var options = "";
 
 
     $(".startbt").on("click", function () {
-        currentQuestion = questions[questionIndex].q
+        //currentQuestion = questions[questionIndex].q
         $('.container-main1').hide();
         $('.container-main2').show();
 
-
-
-        $("#questionBox").append(questionDiv)
-       
-
-        questionDiv.text(currentQuestion)
-
-        for (i=0; i<questions.length; i++){
-            // inside here create buttons, 
-            // set button text to a,b,c inside array
-            // set onclick events
+        var j = 0;
+        var printQ = function () {
+            for (var i = 0; i < questions[j].a.length; i++) {
+                $('#question').html(questions[j].q)
+                $('#options').append(`<button>${questions[j].a[i]}</button>`)
+            }
         }
+        printQ();
 
+        $('button').on('click', function (event) {
+            var click = event.target
+            if (click.innerHTML != questions[j].ans) {
+                $("#result").text("you lose")
+                j++
+                $('#options').empty()
 
+                printQ()
 
-// check to see if the text or data within the button of the event clicked !== question[questionIndex].ans then display or alert "WRONG" and move to next question. that is when you would do questionIndex++
+            } else {
+                $("#result").text("you win!")
+                j++
+                $('#options').empty()
+                printQ()
+            }
+        })
 
-
-
-
-
-
-
-
-        // display question
-        // set timer
-        // if timer runs out, pull next question
-
-
-    });
-
-
-
-
+    })
 
 
 
 
+    // inside here create buttons, 
+    // set button text to a,b,c inside array
+    // set onclick events
 
-});
+
+    //$("#questionBox").append(questionDiv)
+
+
+
+
+
+
+    // check to see if the text or data within the button of the event clicked !== question[questionIndex].ans then display or alert "WRONG" and move to next question. that is when you would do questionIndex++
+
+
+
+
+
+
+
+
+    // display question
+    // set timer
+    // if timer runs out, pull next question
+
+
+
+})
